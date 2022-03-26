593d674cdd9c8dc0d03d30328c170e841bcfb8ec
Peter Gil Villar <villarpeter@gmail.com>
2022-03-26 15:27:04 +0700

    Add sample unit test

---
+++ EmployeeManagementAppBlazorServerTests/Repositories/DepartmentRepositoryTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManagementAppBlazorServer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagementAppBlazorServer.Data;
+using Moq;
+using Microsoft.Extensions.Logging;
+using EmployeeManagementAppBlazorServer.Models;
+
+namespace EmployeeManagementAppBlazorServer.Repositories.Tests
+{
+    [TestClass()]
+    public class DepartmentRepositoryTests
+    {
+        private Mock<ILogger<DepartmentRepository>> _mockLogger = new Mock<ILogger<DepartmentRepository>>();
+
+        private EmployeeManagementDbContext _appDbContext;
+        private IDepartmentRepository _instance;
+
+        public DepartmentRepositoryTests()
+        {
+            _mockLogger.SetupAllProperties();
+
+            var options = new DbContextOptionsBuilder<EmployeeManagementDbContext>()
+            .UseInMemoryDatabase(databaseName: "EmployeeManagementDbMemory")
+            .Options;
+
+            _appDbContext = new EmployeeManagementDbContext(options);
+
+            _instance = new DepartmentRepository(_appDbContext, _mockLogger.Object);
+        }
+
+        [TestMethod()]
+        public async Task AddAsyncTest()
+        {
+            //arrange
+
+            //act
+            var result = await _instance.AddAsync(new Department { Name = "name", Description = "description" });
+
+            //asserts
+            Assert.IsNotNull(result);
+            Assert.AreEqual("name", result.Name);
+            Assert.AreEqual("description", result.Description);
+        }
+    }
+}
